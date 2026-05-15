145743d47c893891a86c4dac5beeb60032c24f9d
Muluken Seifu <mulerseifu87@gmail.com>
2026-05-15 14:12:51 +0300

    define the fields

--- gui/MainMenuPanel.java
+++ gui/MainMenuPanel.java
@@ -1,5 +1,17 @@
 package Chess.gui;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
 
 public class MainMenuPanel {
+    private JButton btnPvP;
+    private JButton btnPVB;
+    private JButton btnSettings;
+    private JLabel titleLabel;
+    private JComboBox difficultyBox;
+
+
+    
     
 }
