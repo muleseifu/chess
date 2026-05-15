b904dd786f2dfb965d87efa14baa16b81e002352
Abere <aberechanie@gmail.com>
2026-05-15 19:08:51 +0300

    created the the structure

--- model/board/Cell.java
+++ model/board/Cell.java
@@ -1,5 +1,68 @@
 package Chess.model.board;
-
+import javax.swing.*;
+import Chess.model.pieces.Piece;
+import 
 public class Cell {
+    private boolean isPossibleDestination;
+    private JLabel content;
+    private Piece piece;
+    private boolean isSelected;
+    private boolean isCheck;
+    private int x,y;
+    //private Color baseColor;
+    public void setPiece(){
+
+    }
+     public void select(){
+
+    }
+     public void deselect(){
+
+    }
+     public void isSelected(){
+
+    }
+     public void removePiece(){
+
+    }
+    public Piece getPiece(){
+        return piece;
+    }
+    public void setPossibleDestination(){
+        
+    }
+    public void removePossibleDestination(){
+        
+    }
+    public boolean isPossibleDestination(){
+        return isPossibleDestination;
+    }
+    public void setCheck(){
+        
+    }
+    public void removeCheck(){
+        
+    }
+    public boolean isCheck(){
+        return isCheck;
+    }
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+    public boolean isEmpty(){
+        return piece == null;
+    }
+    public boolean isEnemy(int color){
+        
+
+    }
+    public void updateSprite(){
+
+    }
+    
+
 
 }
--- model/pieces/Piece.java
+++ model/pieces/Piece.java
@@ -3,5 +3,12 @@ package Chess.model.pieces;
 public class Piece {
     private int One;
     private int Two;
-    
+    private int three;
+    public Piece(int one, int two, int three) {
+        One = one;
+        Two = two;
+        this.three = three;
+    }
+
+  
 }
