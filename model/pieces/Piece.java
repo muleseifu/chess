21bc1ae31308cbadba9e653fe464e88e241559db
YomiyuAddisu <jyomfumorinka@gmail.com>
2026-05-15 02:03:59 -0700

    Piece updateed

--- model/pieces/Piece.java
+++ model/pieces/Piece.java
@@ -1,5 +1,55 @@
 package Chess.model.pieces;
 
-public class Piece {
-    
+public abstract class Piece {
+    protected int color;
+    protected String id;
+    protected String imagePath;
+    protected boolean availablity;
+    protected boolean hasMoved;
+
+
+    public void SetId(String id) {
+        this.id = id;
+    }
+
+    public void SetPath(String Path) {
+        this.imagePath = Path;
+    }
+
+    public void SetColor(int color) {
+        this.color = color;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPath() {
+        return imagePath;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public boolean isAvailable() {
+        return availablity;
+    }
+
+    public void setAvailable(boolean available) {
+        this.availablity = available;
+    }
+
+    public void setMoved() {
+        this.hasMoved = true;
+    }
+
+    public boolean hasMoved() {
+        return hasMoved;
+    }
+
+    public abstract Piece getCopy();
 }
+
+
+
