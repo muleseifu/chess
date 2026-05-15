e8e076846650b6f9c6efcb45752d7df87f30ca85
Abere <aberechanie@gmail.com>
2026-05-15 11:31:45 +0300

    Refactor Piece class: add constructor, standardize setters, remove duplicate getter and debug method

--- model/pieces/Piece.java
+++ model/pieces/Piece.java
@@ -1,54 +1,56 @@
 package Chess.model.pieces;
 
-public abstract  class Piece {
+public abstract class Piece {
     protected int color;
-    protected String id ;
+    protected String id;
     protected String imagePath;
     protected boolean hasMoved;
     protected boolean availability;
-    public void SetId(String id){
+
+    public Piece(int color, String id, String imagePath) {
+        this.color = color;
+        this.id = id;
+        this.imagePath = imagePath;
+        this.hasMoved = false;
+        this.availability = true;
+    }
+
+    public void setId(String id) {
         this.id = id;
     }
-    public void SetColor(int color) {
+
+    public void setColor(int color) {
         this.color = color;
     }
-    public void SetPath(String imagePath) {
+
+    public void setPath(String imagePath) {
         this.imagePath = imagePath;
     }
+
     public void setAvailable(boolean availability) {
         this.availability = availability;
     }
-     public void setMoved() {
+
+    public void setMoved() {
         this.hasMoved = true;
     }
+
     public int getColor() {
         return color;
     }
+
     public String getId() {
         return id;
     }
-    public String getID()
-    {
-        return id;
-    }
-    
+
     public String getPath() {
         return imagePath;
     }
+
     public boolean isAvailable() {
         return availability;
     }
+
     public boolean hasMoved() {
         return hasMoved;
     }
-  public void test() {
-        System.out.println("This is a test method in the Piece class.");
-    }
-   
-
-
-
-
-
-    
-}
