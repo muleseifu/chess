03a750e56d2735f9940a11901a419c095a4b252b
Muluken Seifu <mulerseifu87@gmail.com>
2026-05-21 14:04:59 +0300

    rmv

--- gui/BoardListener.java
+++ gui/BoardListener.java
@@ -1,12 +1,6 @@
 package Chess.gui;
 
-<<<<<<< HEAD
+
 public interface  BoardListener {
     void onBoardChanged();
-=======
-public interface BoardListener {
-
-    public void onBoardChanged();
->>>>>>> 0d448ca3f9ee6e18b568d38ec8b70dd9e8a066ed
-    
 }
